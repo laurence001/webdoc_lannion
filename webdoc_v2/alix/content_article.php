8c68437fc55be4e9b11fdc5c50e56396e3a40bba
Laurence Dierickx <laurence@ohmybox.info>
2021-01-15 09:20:13 +0100

    Add files via upload

---
+++ webdoc_v2/alix/content_article.php
@@ -0,0 +1,9 @@
+<p> 
+	Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.
+</p> 
+				
+<h3 class="inter">Intertitre du texte ici</h3>
+				
+<p> 
+	Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.
+</p> 
---
+++ webdoc_v2/charlie/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html class="no-js" lang="FR=fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Les visages du sport</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php require_once('metadata.php'); ?>
+  <?php require_once('../favicons.php'); ?>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script> 
+  <script src="https://kit.fontawesome.com/5d803a5a54.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&family=Pacifico&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/main.css">
+</head>
+<body id="cat" class="charlie-cat inside cat">
+ 
+	<?php 
+		require_once('../reseaux.php');
+		require_once('navbar.php'); 
+	?>
+    
+    <div class="container main-container">
+		<div class="row"> 
+            
+			<div class="col col-lg-2 col-md-2 col-sm-2 col-6" id="catperso"> 
+              <img src="../img/Charlie.png" alt="charlie"> 
+            </div>
+            
+            <div class="col col-lg-4 col-md-4 col-sm-6 col-6">
+               <div class="cadre">    
+					<p> J’ai 20 ans. Contrairement à beaucoup de mes amies, je n’ai pas arrêté le sport jeune, malgré les pressions sociales. J’ai été agressée, mais mon club n’a rien fait. Je n’ai pas voulu tout arrêter, alors j’ai cherché un nouveau sport. J’ai rejoint un club de boxe féminine, pour être plus forte. </p>
+					<h4>Charlie</h4>	
+				</div>
+				<div class="resp">
+					<div class="center">
+						<img src="../img/charlie.png" alt="charlie"> 
+					</div>
+				</div>
+            </div>
+        
+            <div class="col col-lg-6 col-md-6 col-sm-12 col-12" id="categories">
+              
+			  <section>
+				<h2><a href="article">Les femmes pratiquent de plus en plus de sports de combat</a></h2> 
+				<div class="chapo">
+					En France, 86 % des femmes affirment avoir déjà été agressées. Pour apprendre à se défendre et à avoir plus confiance en elles, une part 
+					croissante se tourne vers les sports de combat ces 20 dernières années. Boxe, karaté, judo ou encore self défense, cette 
+					augmentation des licenciées se démarque de celle des autres sports.
+				</div> 
+			  </section>
+
+			  <section>
+				<h2><a href="article">À Lannion, la parole des sportifs se libère</a></h2>
+				<div class="chapo">  
+				À Lannion, dans les Côtes d’Armor, les médailles et les sourires des jeunes sportif.ve.s
+				brillent dans la presse locale. Pourtant, plusieurs cas d’agressions et de harcèlement sexuel
+				au sein de clubs demeurent inconnus. Ces affaires sont, au moins, ignorées des médias et du
+				public, au pire, étouffées par les associations sportives. Aujourd’hui, la parole des victimes se
+				libère progressivement. Les conséquences, elles, perdurent.
+				</div>
+			   </section>
+
+			   <section>
+				<h2><a href="article">Les clubs de sport Lannionais font barrage aux femmes</a></h2> 
+				<div class="chapo">
+					Une équipe de football féminine a été créée il y a trois ans à Lannion, dans les Côtes-d’Armor. Mais depuis plus rien. Les femmes font face à de multiples difficultés pour pratiquer un sport.
+				</div>
+			   </section>
+			  
+			  <section>				  
+				<h2><a href="article">La puberté, un coup d'arrêt dans la pratique sportive des adolescentes</a></h2>
+				<div class="chapo">  
+					69% des jeunes filles de 6-11 ans font du sport en club, contre 53 % chez les 12-17 ans. Un décrochage au moment de l'adolescence qui ne se retrouve pas chez leurs homologues masculins et qui se poursuit à l'âge adulte. Changement des corps, socialisation genrée dans la pratique sportive et appropriation masculine des clubs. Autant d’éléments qui poussent les adolescentes à arrêter le sport durant la puberté.
+				</div>
+			   </section>
+            </div>
+            
+        </div>
+	</div>
+	
+		<footer id="footer">
+			<div class="container">
+				J2 - InfoCom Lannion &copy; 2021
+			</div>
+		</footer>
+		
+		<script src="../js/vendor/modernizr-3.11.2.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/main.js"></script>
+    </body> 
+</html>
