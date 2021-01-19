3e64671f88208d208581aebfcaa1b65a388226cd
Laurence Dierickx <laurence@ohmybox.info>
2021-01-19 06:36:39 +0100

    Delete webdoc_v2 directory

--- webdoc_v2/aylan/content_article.php
+++
@@ -1,9 +0,0 @@
-<p> 
-	Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.
-</p> 
-				
-<h3 class="inter">Intertitre du texte ici</h3>
-				
-<p> 
-	Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop. Texte ayant un l'air un peu biedon mais pas trop.
-</p> 
