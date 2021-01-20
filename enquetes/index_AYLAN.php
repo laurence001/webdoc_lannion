d553a8126ce8c0c3e3d2ecaa20ca1afe77455404
Maelennico <72437534+Maelennico@users.noreply.github.com>
2021-01-20 11:19:13 +0100

    Add files via upload

---
+++ enquetes/index_AYLAN.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js" lang="FR=fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Les visages du sport</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php require_once('metadata.php'); ?>
+  <?php require_once('../favicons.php'); ?>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script> 
+  <script src="https://kit.fontawesome.com/5d803a5a54.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&family=Pacifico&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../css/new.css">
+</head>
+<body id="cat" class="aylan-cat inside cat">
+ 
+	<?php 
+		require_once('../reseaux.php');
+		require_once('navbar.php'); 
+	?>
+    
+    <div class="container main-container">
+		<div class="row"> 
+            
+			<div class="col col-lg-2 col-md-2 col-sm-2 col-6" id="catperso"> 
+              <img src="../img/Aylan.png" alt="aylan"> 
+            </div>
+            
+            <div class="col col-lg-4 col-md-4 col-sm-6 col-6">
+               <div class="cadre">    
+					
+					<h4>Aylan</h4>	
+					
+					<p> Cela fait 31 ans que j’habite dans un quartier populaire, j’y ai grandi. Sportif depuis mon plus jeune âge, je me rends désormais compte du sous-développement de la pratique sportive dans les quartiers. Une fracture économique avec le reste du pays, d’autant plus que certains sports “de riches” sont inaccessibles pour des gens comme moi. </p>
+					
+				</div>
+				<div class="resp">
+					<div class="center">
+						<img src="../img/aylan.png" alt="aylan"> 
+					</div>
+				</div>
+            </div>
+        
+            <div class="col col-lg-6 col-md-6 col-sm-12 col-12" id="categories">
+              
+			  <section>
+				<h2><a href="content_quartierspopulaires.php">Les quartiers populaires, si loin du sport</a></h2> 
+				<div class="chapo">
+					Dans les quartiers populaires, la pratique du sport est bien moins développée que dans le reste de la France. Manque d’équipements, licences trop chères, problèmes de transports… Enquête dans les deux quartiers prioritaires de Lannion, en Bretagne.
+				</div> 
+			  </section>
+
+			  <section>
+				<h2><a href="content_riche.php">Equitation, Golf, Aviation… Quand la compétition est réservée aux plus riches</a></h2>
+				<div class="chapo">  
+				Équitation, golf, voile ou encore ski, tous sont considérés comme réservés aux personnes aisées. Certains tentent de se défaire de cette réputation. Le résultat de ces initiatives est mitigé : au-delà du coût, la culture élitiste de certains sports est un facteur d’exclusion.
+				</div>
+			   </section>
+
+            </div>
+            
+        </div>
+	</div>
+	
+		<footer id="footer">
+			<div class="container">
+				J2 - InfoCom Lannion &copy; 2021
+			</div>
+		</footer>
+		
+		<script src="../js/vendor/modernizr-3.11.2.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/main.js"></script>
+    </body> 
+</html>
