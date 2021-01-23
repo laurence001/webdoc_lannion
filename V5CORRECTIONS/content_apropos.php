6e5d322abfec59f014e3c3e2fc08841c4587ed1e
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-23 17:17:04 +0100

    Add files via upload

---
+++ V5CORRECTIONS/content_apropos.php
@@ -0,0 +1,11 @@
+Contenu page à propos kdjkjd kjdkjdk dkjkdd
+
+<h4 class="inter">Un nouveau titre</h4>
+
+
+<p>
+	Suite d emon <strong>texte</strong>
+</p>
+
+<img src="img/monimage.jpg" alt="La rédaction J2 Lannion" />
+
---
+++ V5CORRECTIONS/content_index.php
@@ -0,0 +1,41 @@
+<div class="description" id="sasha">
+	<h4>Sasha</h4>
+	<p>
+		J’ai 20 ans. Contrairement à beaucoup de mes amies, je n’ai pas arrêté le sport à mon adolescence. J’ai été agressée, mais mon club n’a rien fait. Je n’ai pas voulu tout arrêter, alors j’ai cherché un nouveau sport. J’ai rejoint un club de boxe féminine, en partie pour (re)gagner confiance en moi. 
+	</p>
+</div>
+
+<div class="description" id="charlie">
+	<h4>Charlie</h4>
+	<p>
+		J’ai fêté mes 70 ans cette année. J’ai eu un cancer il y a quelques années. Le sport m’a permis de m’en sortir et aujourd’hui je suis totalement guéri. Je profite de ma retraite pour me baigner dans les eaux bretonnes le plus souvent possible. J’y retrouve des habitué·e·s,  cela m’aide à rester en forme et à garder le moral.  
+	</p>
+</div>
+
+<div class="description" id="alix">
+	<h4>Alix</h4>
+	<p>
+		J’ai 40 ans cette année. J’ai toujours été passionnée par le vélo. Il y a quelques années, j’ai eu un accident, un événement très commun chez les cyclistes. Plus tard, j’ai cherché des alternatives à ma pratique sportive en me tournant vers les innovations électriques et les clubs handisports, malheureusement très peu développe·é·s dans ma région. 
+	</p>
+</div>
+
+<div class="description" id="elio">
+	<h4>Elio</h4>
+	<p>
+			Cela fait 31 ans que j’habite dans un quartier populaire, j’y ai grandi. Sportif depuis mon plus jeune âge, je me rends désormais compte du sous-développement de la pratique sportive dans les quartiers. Une fracture économique avec le reste du pays, d’autant plus que certains sports “de riches” sont inaccessibles pour des gens comme moi.
+	</p>
+</div>
+
+<div class="description" id="aylan">
+	<h4>Aylan</h4>
+	<p>
+				Je suis élu·e au conseil municipal de ma commune depuis 15 ans. Chargé·e des sports, j’ai 56 ans et jai une pratique assidue de vélo et de natation. Je me bat tous les jours pour développer le budget des associations et clubs sportifs de ma ville. Même si je reste passionné·e, je suis parfois lassé du manque de moyens financiers, d’infrastructures, et la baisse des adhérents. 
+	 </p>
+</div>
+
+<div class="description" id="intro">
+	<p>
+	Saviez-vous que dans les Côtes d’Armor (Bretagne), un entraîneur accusé de violences sexuelles exerce toujours ? Ou que les clubs de sport souffrent de la crise sanitaire ? A travers 14 enquêtes, c’est ce qu’expliquent Sasha, Charlie, Aylan, Elio et Alix. 
+	Ces cinq personnages fictif·ve·s aux histoires de vie bien différentes ont un point commun : leur passion pour le sport. Retraité, Charlie est en bonne santé grâce à sa pratique sportive. De son côté,  Alix concourt en catégorie handisport depuis une grave chute de vélo. Sasha et Elio sont deux jeunes adultes : l’une a été victime de violence sexuelles dans son club ; l’autre vit dans un quartier populaire, en proie aux inégalités. Aylan est quant à lui conseiller municipal pour le sport dans un petit village de Bretagne.
+	</p>
+</div>
