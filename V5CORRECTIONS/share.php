143bcb807d5f3572dcccdfc57bd943a71b27dc4c
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-23 17:16:10 +0100

    Add files via upload

---
+++ V5CORRECTIONS/share.php
@@ -0,0 +1,27 @@
+<ul class="share">
+	<li>
+		<a href="">
+			<i class="fab fa-twitter"></i>
+		</a>
+	</li>
+	<li>
+		<a href="">
+			<i class="fab fa-facebook"></i>
+		</a>
+	</li>
+	<li>
+		<a href="">
+			<i class="fab fa-pinterest"></i>
+		</a>
+	</li>
+	<li>
+		<a href="">
+			<i class="fa fa-linkedin"></i>
+		</a>
+	</li>
+	<li>
+		<a href="">
+			<i class="fab fa-whatsapp"></i>
+		</a>
+	</li>
+</ul>
