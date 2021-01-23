86c2d6af3a7585749fe04a043ff3a6316869d44c
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-23 18:53:31 +0100

    Add files via upload

---
+++ V5CORRECTIONS/sasha/bottom.php
@@ -0,0 +1,23 @@
+<div class="container">
+	<div class="col-12" id="bottom">
+		<figure id="fig1">
+			<a href="sasha/index"><img src="../img/Sasha.png" alt="Sasha"></a>
+		</figure>
+			
+		<figure id="fig2">
+			<a href="charlie/index"><img src="../img/Charlie.png" alt="Charlie"></a>
+		</figure>
+			
+		<figure id="fig3">
+			<a href="alix/index"><img src="../img/Alix.png" alt="Alix"></a>
+		</figure>
+		  
+		<figure id="fig4">
+			<a href="elio/index"><img src="../img/Elio.png" alt="Elio"></a>
+		</figure>
+
+		<figure id="fig5">
+			<a href="aylan/index"><img src="../img/Aylan.png" alt="Aylan"></a>
+		</figure>
+	</div>
+</div>
---
+++ V5CORRECTIONS/sasha/navbar.php
@@ -0,0 +1,23 @@
+	<header id="topnav">
+		<div class="container">
+            <h1><a href="../">LES VISAGES DU SPORT</a></h1>
+		</div>
+    </header>
+	 
+	<nav class="navbar">
+		<div id="close">
+			<i class="fas fa-times"></i>
+		</div>
+		
+		<div class="resp">LES VISAGES DU SPORT</div>
+		
+		<ul class="menu">
+			<li><a href="../">Accueil</a></li>
+			<li id="nav_sasha" class="active"><a href="index">Sasha</a></li>
+			<li id="nav_charlie"><a href= "../charlie/index">Charlie</a></li>
+			<li id="nav_alix"><a href= "../alix/index">Alix</a></li>
+			<li id="nav_elio"><a href= "../elio/index" id="Elio">Elio</a></li>
+			<li id="nav_aylan"><a href= "../aylan/index">Aylan</a></li>
+			<li class="separator"><a href= "../apropos">A propos</a></li>
+		</ul>
+    </nav>
