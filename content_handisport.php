1bac591da2dcb1e1fcf42b4f4e3f86959942c902
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-24 17:21:24 +0100

    Add files via upload

---
+++ content_handisport.php
@@ -0,0 +1,61 @@
+<p>L'entraînement du club trégorrois handisport de Lannion est bien calme. Pas plus de 10 joueur·euses, soit juste assez pour faire un match. Cette situation marque un réel manque d’effectif. Lors du discours de politique générale en juillet 2017, le Premier ministre Edouard Philippe voulait voir « augmenter le nombre de personnes en situation de handicap pratiquant une activité physique et sportive et ce, dès le plus jeune âge ». Depuis 2020, les ministères des Sports et du Handicap ont fusionné pour « pallier au mieux les problématiques du  handisport ».<p/>
+<p>La pratique d’une activité physique permet aux personnes handicapées de lutter contre le repli sur soi et l’isolement. Dans les Côtes-d’Armor, en 2018, 7,3 % de la population était porteuse de handicap, selon les services du conseil départemental. Ce chiffre considérable pourrait laisser penser que le territoire dispose des structures et des financements nécessaires à l’adaptation des infrastructures.<p/>
+<p>Or, d’après la carte proposée par le Comité départemental handisport du territoire, les clubs sont peu nombreux : 324 licenciés pour 6 clubs et 15 sections handisports. Différence de taille : une section handisport appartient à un club valide, alors qu’un club handisport se consacre essentiellement à l'entraînement de personnes non-valides. Du handibasket à Lannion ou Plaintel, un club de tennis de table à Loudéac, un club de sarbacane à Dinan…  Ces clubs, installés aux quatre coins du département, contraignent les personnes non-valides à disposer d’un moyen de transport régulier.<p/>
+<h3 class="inter">Les subventions, le nerf de la guerre</h3>
+<p>Aides insuffisantes, infrastructures inadaptées, matériel onéreux… Les obstacles au développement du handisport sont nombreux et les associations sportives ont du mal à s’adapter. La pérennité des clubs handisport est conditionnée aux financements des collectivités et de l'Agence nationale du sport (ANS). Le handibasket est une discipline très onéreuse. Les fauteuils utilisés au quotidien par les joueur·euse·s ne sont pas homologués pour la pratique sportive. Les sportif·ve·s doivent alors acheter des équipements adaptés à leur morphologie pour assurer leur sécurité et augmenter leurs performances.<p/>
+<p>D’après Gérard Guivarch, président du Club trégorrois handisport de Lannion, le coût d’un fauteuil est estimé entre « 2 000 et 7 000 euros selon le gabarit du joueur ». Ce coût considérable contraint le club lannionnais comme la section handibasket de Plaintel à demander des subventions aux collectivités. Pour la section handisport de Plaintel, l’enveloppe annuelle ne dépasse pas les 250 euros. Des aides jugées insuffisantes par les président·e·s d’association, alors obligé·e·s de faire appel au sponsoring : « Le club survit grâce aux dons que peut faire Decathlon, c’est dommage d’en arriver là… », regrette Sébastien Hadengue, président de la section.</p>
+<p>Dans les mairies, ce sont les commissions qui discutent de la répartition des subventions. « L’enveloppe est calculée selon le nombre d'adhérents, la capacité d’accueil et le type des besoins », explique Sylvie Thomas, membre du secrétariat de direction de la communication de Plaintel. Fonctionnement qui laisse Sébastien Hadengue perplexe. « Si on s’arrête aux nombres d’adhérents, on ne s’arrête pas au coût de la pratique. Et les calculs ne sont pas bons ! ».</p>
+<p>Gwenaël Lair, nouvelle adjointe aux politiques sportives et handicaps de Lannion, estime sa ville « chanceuse » d’être aussi équipée en infrastructures handisports. Une vision que ne partage pas le président du club de handibasket lannionnais : « Nous avons demandé depuis 3 ans de refaire le sol du gymnase pour qu’il soit plus lisse, car la surface actuelle provoque trop de blessures ». Coût total de l’opération : 100 000 euros. Bien que ces joueur·euse·s évoluent en Nationale A, le plus haut de la compétition en France, le sentiment général est que les problématiques handisports ne sont pas dans les priorités de la collectivité. Jean-Louis Legros, président de la section handisport d’escrime lannionnaise, regrette aussi ce manque de considération. Créé en 2015, le club ne dispose toujours pas de vestiaires adaptés aux fauteuils roulants.</p>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="../img/handisport2.jpg" alt="Carrousel slide 3" class="d-block w-100">
+          </div>
+          <div class="carousel-item">
+            <img src="../img/handisport3.jpg" alt="Carrousel slide 3" class="d-block w-100">
+          </div>
+          <div class="carousel-item">
+            <img src="../img/handisport4.jpg" alt="Carrousel slide 3" class="d-block w-100">
+          </div>
+		    <div class="carousel-item">
+			<img src="../img/handisport5.jpg" alt="Carrousel slide 3" class="d-block w-100">
+		  </div>
+		  
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h3 class="inter">Un manque de clubs remarquable </h3>
+<p>« J’ai toujours voulu faire de la danse, mais le seul club adapté était à 37 km de chez moi. Mes parents travaillent. Ils ne pouvaient pas faire autant de route le soir », confie Marine, 13 ans, autiste. Elle est loin d’être un cas isolé : des dizaines d’enfants rencontrent le même problème. Selon le comité départemental handisport des Côtes d’Armor, un·e enfant invalide sur cinq renonce à la pratique d’une activité physique en raison du défaut d’accessibilité à un club. Pour Gaëlle Lozac’h, présidente du comité : « Il faut former davantage de personnes pour développer l’offre de club sur le secteur ». C’est un manque que des parents d’enfants invalides ont cherché à combler dès 2015 : attristé·e·s que leurs enfants ne puissent faire du sport dans un club de proximité, iels ont décidé de créer une section handibasket à Plaintel.</p>
+<p>D’autres parents ont développé des initiatives pour pallier ce déficit, comme Frédéric Tual, père d’un·e enfant handicapé·e et développeur informatique. Pour lui, le défaut de formation et le manque de clubs sont intimement liés. Depuis 2017, ces parents travaillent avec les collectivités pour développer leur offre handisport. Il a créé une plateforme en ligne, en partenariat avec le service Bretagne Handicap, qui informe sur les protocoles à mettre en place. Les parents d’enfants atteint·e·s de handicap ont le sentiment que leurs ambitions ne sont pas partagées par les institutions. « Le renforcement de l’inclusion des personnes en situation de handicap par le sport est une préoccupation majeure du ministère », déclarait pourtant Roxana Maracineanu, ministre chargée des Sports, lors d’une conférence de presse le 5 octobre 2020.</p>
+<h3 class="inter">Jeux paralympiques 2024, quelles perspectives ?</h3>
+<p>La France accueillera les Jeux paralympiques 2024. « Nous avons donné les moyens au Comité paralympique et sportif français pour pouvoir employer une personne qui fait le lien entre le monde associatif sportif, les collectivités et le champ du handicap », a annoncé la ministre des Sports. Pour le club professionnel de Lannion, les éléments ressemblent à un effet d’annonce, comme pour les jeux de 2012 et 2016 : « À Rio ou à Londres, cela n’a pas changé la situation financière du handisport. »</p>
+<p>Les revendications des associations sont pourtant claires : « Plus de médiatisation, plus de subventions et arrêter les discours électoralistes ». À l'avenir, les associations sportives espèrent une véritable politique d’intégration : pour toustes ces sportif·ves·s, les Jeux paralympiques ont bien plus à offrir que de simples médailles.</p>
