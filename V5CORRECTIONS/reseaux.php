7bf0b0c3dca951cebb3737221f8e95a5138e25ab
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-24 16:14:01 +0100

    Delete V5CORRECTIONS directory

--- V5CORRECTIONS/reseaux.php
+++
@@ -1,8 +0,0 @@
-	<ul id="reseaux">
-		<li><a href="" target="_blank"><i class="fab fa-twitter"></i></a></li>
-		<li> <a href="" target="_blank"><i class="fab fa-instagram"></i></a></li>
-	</ul>
-
-	<div id="burger">
-		<i class="fas fa-bars"></i>
-	</div>
