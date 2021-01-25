7ec55c06b2e8981ed45901486e558031b5b6514a
Paulouault <71820078+Paulouault@users.noreply.github.com>
2021-01-25 10:29:17 +0100

    Add files via upload

---
+++ content_milieurural.php
@@ -0,0 +1,38 @@
+<p> Dans les Côtes-d’Armor, trouver un club de sport s'apparente souvent au parcours du combattant. « Devoir faire dix km pour venir à l'entraînement, c’est embêtant », témoigne Antoine Boyer, joueur de handball à l’Amicale Laïque Trébeurden. Depuis ses trois ans, le jeune homme fait du handball. Auparavant, il jouait à Montpellier où il n’avait aucun mal à trouver un club. Mais pour des raisons personnelles, Antoine et sa famille ont dû déménager dans les Côtes-d’Armor, territoire plus rural. La famille découvre alors la difficulté de trouver un club pour leur enfant.  gé de quinze ans, Antoine doit prendre la route pour rejoindre son club. « Quand j’étais jeune, c’était mes parents qui faisaient le taxi. Maintenant, c’est moi le taxi, étant donné qu’un de mes coéquipiers n’a pas de voiture », remarque le jeune handballeur.
+</p> 
+
+<p>	Puisque Lannion n’a plus d’équipe de handball, il faut se rendre à Trébeurden, à dix kilomètres, pour trouver le club le plus proche. « Nous avons dû réunir trois communes pour trouver les moyens et les effectifs suffisants afin d’exister », explique Géraldine Le Masson, présidente de l’Amicale Laïque Trébeurden. Ce système d’union appelé “entente” est la solution la plus développée dans le milieu rural armoricain pour lutter contre le manque d'infrastructures et de moyens des petites communes. « Les ententes permettent de partager les coûts entre les communes, et les clubs peuvent utiliser les infrastructures des collectivités. C’est un système gagnant-gagnant », ajoute Géraldine Le Masson.
+ </p>
+
+<h3 class="inter">Chute des revenus de sponsoring</h3>
+
+<p> 	Les Côtes d’Armor sont un département très rural, avec 106 communes de moins de 500 habitant·e·s. La majorité de la population se concentre autour de villes moyennes et/ou des zones d’emploi : Lannion, Guingamp, Loudéac, Saint-Brieuc et Dinan. La population est concentrée surtout dans le nord du département, Loudéac étant le seul bassin de vie au sud. Serge Falezan, bénévole de la Fédération Nationale du Sport en Milieu Rural (FNSMR), regrette que les jeunes se tournent plus facilement vers les villes : « ils vont tous à Lannion ou Saint-Brieuc ».
+</p> 
+
+<p> 	Le nord est plus urbanisé que le sud et la mobilité est facilitée par la Nationale 12. La grande majorité des clubs sportifs créent des ententes dans les zones urbaines. Une solution pour les associations de sport amateur est donc de former des ententes. L’objectif étant principalement de mutualiser les coûts, pour couvrir les besoins de gestion : c’est une manière de continuer à se développer et à fédérer les pratiquants d’une même localité. Dans un département où le véhicule personnel est le moyen de déplacement privilégié, les personnes n’ayant pas de permis de conduire ou de véhicule personnel sont donc moins susceptibles de pratiquer une activité sportive. « Il y a de moins en moins de clubs en Centre Bretagne, souligne Serge Falezan. Il y avait beaucoup de clubs de foot avant, mais ils disparaissent de plus en plus ou alors font des ententes. »
+</p>
+
+<p> 	Autre disparité : ce sont les territoires côtiers qui comptent le plus grand nombre d’équipements sportifs. Néanmoins, ces équipements sont particulièrement dédiés aux sports nautiques. De leur côté, les sports de nature ou l’athlétisme sont peu développés dans le département. Parmi les 115 disciplines sportives organisées par une fédération sportive à l’échelon national, seules 70 y sont représentées et une cinquantaine sont fédérées par le CDOS (Comité départemental olympique et sportif).
+</p>
+
+<h3 class="inter">Des solutions à demi-teinte</h3>
+
+<p>Les Côtes d’Armor sont le département breton ayant la plus faible concentration en infrastructures sportives, en comparaison à sa population. « Des zones blanches sportives », que dénonce Serge Falezan, bénévole de la FNSMR 22 : « les infrastructures sont trop onéreuses, il est impossible d’investir dans un gymnase pour les petites communes ». Face à ce problème, le gouvernement propose l’installation de terrains multisports (« city stade »). Mais cette solution ne favorise pas la création d’associations et ne résout pas les problèmes de financement. « Il n’y pas de bonus à la ruralité en matière d’équipement sportif , on essaye d’être vigilant à ce que tout ne se fasse pas dans les zones urbaines », avoue, du bout des lèvres, Pierres-Yves Mouy, chef de la section sport et jeunesse des Côtes-d’Armor.<p/>
+
+<p> 	Dans les années 60, l’Etat a encouragé l'installation de stades de foot communaux. Cette politique, qui a connu un important succès en Côtes d’Armor, est devenue obsolète. « Aujourd’hui, il y a toujours les terrains de foot mais à la place des joueurs, il y a des vaches, faute d’équipe », dénonce Pierre-Yves Mouy. Les petites communes font face à des problèmes d’effectifs au niveau des encadrant·e·s et des joueurs·ses. La parade a été de créer des ententes intercommunales. « Je vais avoir 46 ans et il y a toujours eu des ententes à Plumieux », témoigne Sébastien Quinio, maire de Plumieux, une ZRR (Zone de Revitalisation Rurale). Mais les ententes avec leur système “gagnant-gagnant” oblige les joueurs·ses à devoir se déplacer de commune en commune pour s'entraîner.
+</p>
+
+<h3 class="inter">“Le sport n’est pas une priorité” </h3>
+
+<p> 	Près d’un tiers des communes des Côtes d’Armor sont classées en ZRR (Zone de revitalisation rurale).<p/>
+
+<iframe width="100%" height="520" frameborder="0" src="https://mariaaze.carto.com/builder/5407bb99-2574-482a-9efc-1fec54d4ab22/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+    
+<p>« Malheureusement, les maires des ZRR s’occupent plus de la ruralité que de leurs communes. Le sport n’est pas leur priorité », regrette Serge Falezan. « La plupart des communes préfèrent mutualiser avec les communes voisines car construire une salle de sport coûte trop cher pour une commune seule ». Cette mutualisation des salles de sports influe donc aussi sur le nombre de clubs  : la plupart des communes ne possèdent pas assez d’associations sportives pour subvenir aux besoins de sa population.
+</p>
+
+<p> 	Si le Comité Départemental Olympique et Sportif des Côtes d’Armor a pour objectif l’ouverture du sport pour tous·tes, les clubs qui cherchent à obtenir un financement auprès d’eux ont, quant à eux, une ambition de compétition. Ce différend d’objectifs entraîne une baisse significative des dossiers déposés pour les demandes de financement des clubs. En 2018, le département avait recensé 79 demandes pour 59 clubs différents. Ce volume fait des Côtes d’Armor le département ayant formulé le moins de demandes, baissant les  revenus des associations sportives. Les communes et/ou offices des sports sont les principaux·ales acteurs·trices du sport sur ces territoires ruraux.
+</p>
+
+<p> 	La population vieillissante des Côtes d’Armor implique que les pratiques sportives concernent des jeunes et des personnes âgées. Les zones rurales mettent donc en place des jeux sportifs traditionnels bretons, qui peuvent correspondre aux envies d’une partie de la population. Cependant, ces activités sportives ne semblent pas séduire les jeunes, qui les délaissent de plus en plus. « Les jeunes issus de milieu rural ont de moins en moins envie de faire du sport », assure Serge Falezan. Si rien ne change, les zones rurales sont condamnées à regarder leurs infrastructures sportives se dégrader. Les politiques sportives décidées à Paris sont loin de la réalité du terrain.
+</p>
