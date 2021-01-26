52198e9f64283199b45042775efe80e5df999329
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-26 12:55:12 +0100

    Add files via upload

---
+++ content_apropos.php
@@ -0,0 +1,49 @@
+
+<p>
+« Les visages du sport » est un webdocumentaire produit par les étudiant.e.s en deuxième année de journalisme de l'IUT de Lannion (promotion 2019 - 2021). Nous avons enquêté sur les enjeux sociaux, sociétaux et économiques du domaine sportif.   
+<br>
+<br>
+Aujourd'hui, deux Français.e.s sur trois pratiquent un sport au moins une fois par semaine. Mais l’activité. physique ne s'arrête pas seulement à des compétitions sportives. Sommes-nous vraiment toustes égaux dans cette pratique ? À travers cinq profils de citoyen.nes-types fictif.ve.s, de toustes âges, genres et classes sociales, ce webdocumentaire vous emmène dans les réalités de ce milieu. Vous y trouverez 14 enquêtes approfondies et déclinées selon différentes trajectoires de vie.
+<br>
+<br>
+Sasha est une adolescente confrontée aux difficultés d’un monde longtemps réservé aux hommes. Retraité, Charlie est aujourd’hui en bonne santé. Sa pratique sportive l’a aidé à guérir de son cancer. Aylan, conseiller municipal, regrette le peu de moyens financiers et matériels attribué au sport. Elio vient d’un quartier populaire. Face à la réalité des moyens financiers et des politiques locales, sa pratique sportive est limitée. Alix est passionnée par le vélo. Depuis son accident, elle peine à trouver des clubs handisports. 
+<br>
+<br>
+Vous pouvez nous retrouver sur Twitter (@LVDSport) et Instagram (@lesvisagesdusport).
+</p>
+<br>
+<br>
+<img src="img/Covidé.jpeg" alt="La rédaction J2 Lannion" />
+<br>
+<br>
+<p> 
+Rédacteurs.trices en chef : Milan Loustalot et Marion Mergault.
+<br>
+<br>
+Rédacteurs en chef adjoints : Jérémy Fichaux et Manuel Magrez.
+<br>
+<br>
+Intervenante web-développement : Laurence Dierickx.
+<br>
+<br>
+Responsables relecture : Philippe Gestin, Pierre Puchot, et Anna Quéré.
+<br>
+<br>
+Responsables conception : Colyne Emeriau, Germain Michelet, Violette Vauloup, et Victorine Vauquelin.
+<br>
+<br>
+Responsables réseaux sociaux :  Myriam Chevé, Jade Duong, Naomie Jourand, et Quentin-Mathéo Pihour.
+<br>
+<br>
+Responsables développement : Tom Bertin, Fabien Briand, Julie Chauvin, Chloé Crochu, Maël Delmur, Louis Floc’h, Evan Livenais, Paul Louault, Maxime Moulin, Maëlenn Nicolas, Chloé Nivard, et Koupaïa Rault.
+<br>
+<br>
+Responsables enrichissement (audio, vidéo, photo, et infographies) : Maria Azé, Maël Baudé, Chloé Ebrel, et Klervie Vappreau.
+<br>
+<br>
+Rédacteurs.trices : Maria Azé, Maël Baudé, Tom Bertin, Fabien Briand, Julie Chauvin, Myriam Chevé, Chloé Crochu, Maël Delmur, Jade Duong, Chloé Ebrel, Colyne Emeriau, Jérémy Fichaux, Louis Floc’h, Naomie Jourand, Evan Livenais, Milan Loustalot, Paul Louault, Manuel Magrez, Marion Mergault, Germain Michelet, Maxime Moulin, Maëlenn Nicolas, Chloé Nivard, Quentin-Mathéo Pihour, Koupaïa Rault, Klervie Vappreau, Violette Vauloup, et Victorine Vauquelin.
+
+
+
+
+
