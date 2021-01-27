20c28ef829f26677200c71da112d6bcd67700f66
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-27 23:41:24 +0100

    Add files via upload

---
+++ corrections2501/content_quartierspopulaires.php
@@ -0,0 +1,65 @@
+<p> 
+	En bas d’un bâtiment HLM de Ker Uhel, quartier populaire de Lannion, cinq jeunes flânent. «&nbsp;Il n’y a rien d’autre à faire ici&nbsp;», rien d’autre que le sport. Pour autant, les habitant·e·s des 1 500 «&nbsp;quartiers prioritaires de la politique et de la ville&nbsp;» (QPV) sont peu inscrit·e·s dans les structures sportives. Dans ces zones, le taux de licencié·e·s était d’environ 12% contre 27% dans le reste de la France en 2018, d’après les données recensées auprès des fédérations sportives.
+</p>
+
+<p>	
+	En 2013, le ministère des Sports a mis en place une «&nbsp;convention triennale d’objectifs pour les quartiers populaires&nbsp;». L’idée : mieux équiper sportivement ces territoires, grâce à une redistribution plus ciblée des subventions. Sept ans après, les résultats déçoivent. Cette politique n’a pas eu l’effet escompté. Ce sont les mêmes quartiers qui restent, pour la plupart, en marge des équipements sportifs.
+</p>
+				
+<h3 class="inter">«&nbsp;À Ker Uhel, on est loin de tout !&nbsp;»</h3>
+				
+<p> 
+Dans le quartier de Ker Uhel, situé entre le centre-ville et la technopole de Lannion, l’échec est bien visible. Les habitant·e·s de ce quartier «&nbsp;prioritaire&nbsp;» déplorent le manque d’équipements sportifs. “On est loin de tout !”, se plaint Françoise Squeren, membre du conseil citoyen et moteur de la vie du quartier. Les résident·e·s de Ker-Uhel jalousent l’autre QPV de Lannion. Ar Santé Les Fontaines bénéficie d’un city stade, d’un skate-park et d’une base sport nature située à proximité.</p> 
+	
+	<div class="img-text">
+		<img src="../img/quartierspopulaires2.jpg" alt="Le quartier de Ker Uhel à Lannion">
+		<div id="legende">
+			Les deux “QPV” de Lannion. Celui de Ker Uhel à gauche, celui d’Ar Santé Les Fontaines à droite. 
+		</div>
+	</div>
+<p> 
+	À Ker-Uhel, les derniers équipements sportifs construits sont des terrains de tennis. Françoise rit jaune. «&nbsp;Du tennis… Pourquoi pas un golf tant qu’on y est ?&nbsp;». Cette ironie s’explique par le prix élevé des licences : 157 euros pour un·e enfant et 217 euros pour un·e adulte, sans compter le coût des équipements. Pour ce sport comme pour beaucoup d’autres, le montant de l’adhésion démotive la majorité des familles, souvent précaires. À Ker-Uhel, 41,8% des habitant·e·s vivaient sous le seuil de pauvreté en 2017, d’après le Système d'information géographique de la politique de la Ville (SIG Ville). En comparaison, le taux moyen en France est de 14,1%.
+</p>
+<h3 class="inter">Aucune subvention reçue à Lannion</h3>
+<p>
+	Autre frein : le transport. «&nbsp;À Ker-Uhel, il y a beaucoup de familles monoparentales avec plusieurs enfants. C’est compliqué pour les parents de les emmener faire du sport&nbsp;», constate Françoise Squeren. En effet, en 2018, 24,1% des familles sont monoparentales en QPV contre 15,7% dans les autres quartiers, d’après l’Observatoire national de la politique de la Ville (ONPV). Pour soulager les familles, certain·e·s entraîneur·euse·s de clubs viennent chercher les jeunes au quartier. Laurent Guil, professeur de boxe du Trégor Boxing depuis 25 ans, s’y est essayé. Pour lui, le problème est plus profond qu’un manque d’accès aux sports. «&nbsp;Les premières années, je l’ai fait. J’ai constaté que ça ne marchait pas. Les parents ne jouent pas le jeu.&nbsp;»
+	Pour ceux qui se déplacent en transport en commun, c’est tout aussi complexe. La ville de Lannion est globalement mal desservie. Les quartiers “prioritaires” ne font pas exception. Le week-end, aucun bus ne passe à Ker-Uhel. C’est un problème, surtout au vu de la distance qui sépare le quartier des équipements sportifs. En 2018, Ker-Uhel faisait partie des 40 QPV les plus démunis parmi les 1 500 de France, d’après l’ONPV. Il est catégorisé comme “quartier à faible accessibilité”.
+</p>
+	<iframe width="100%" height="300px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/les-quartiers-populaires-si-loin-du-sport_550539?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-quartiers-populaires-si-loin-du-sport_550539">Voir en plein écran</a></p>
+<p>
+	Alors, avant chaque match, Abderrezak, 17 ans, s’impose un drôle d’échauffement : 30 minutes de marche pour rejoindre le stade de Servel Lannion, club de football le plus proche du quartier.
+</p>
+<h3 class="inter">De moins en moins de clubs</h3>
+<p>
+	Avant, il y avait l’ASPTT Lannion à 10 minutes à pied. Le club a fermé en 2002, faute de subventions suffisantes. Il n’est pas le seul. D’après Ouest-France, entre 2 000 et 4 000 clubs de football ont mis la clé sous la porte ces dernières années en France. Grégory Guitton a grandi à Ker-Uhel, il se rappelle de ses années football à l’ASPTT. «&nbsp;C’était un club familial. Les jeunes d’ici, on a grandi avec&nbsp;». Jessy, 13 ans, est déçu quand il apprend qu’un club existait avant sa naissance. «&nbsp;Ça aurait été cool, un club de quartier. On aurait pu jouer tous ensemble ! Je pense que j’aurais pris une licence&nbsp;», détaille l’habitant de Ker-Uhel. Les plus motivé·e·s ont donc quitté le quartier pour continuer à jouer. Les autres ont tout simplement arrêté.
+</p>
+<p>
+	Une question subsiste. Malgré les investissements, pourquoi aussi peu de clubs voient le jour ? 5,5 millions d’euros ont été alloués à des projets sportifs aux 435 contrats de ville en 2017. Lannion bénéficie d’un de ces contrats, pourtant, Michel Lanchec, responsable des sports de la ville, déplore toujours un manque. «&nbsp;Malgré nos demandes, nous n’avons pas touché de subventions pour nous aider à financer nos projets. L’Agence nationale du sport (ANS) a décidé que Lannion était assez doté en équipements sportifs&nbsp;».
+</p>
+<h3 class="inter">Un parkour pour remplacer le city stade</h3>
+<p>
+	Un des seuls moyens pour les enfants de Ker-Uhel de faire du sport, c’est le service des animateurs de quartier. Développé en 2018 par la mairie de Lannion, il emploie six animateur·rice·s qui organisent des ateliers sportifs et culturels. Avec la suspension des entraînements dans les clubs, ils bénéficient enfin d’une salle omnisports. Ce qui n’est pas le cas en temps normal, malgré les accords tenus avec la mairie. «&nbsp;Les clubs et les associations ont le monopole des équipements sportifs à Lannion&nbsp;», regrette Audrey Jolivel, animatrice de quartier. 
+	Pour remettre le sport au cœur de la vie de Ker-Uhel, la ville de Lannion a décidé de construire un parkour park. Prévu pour début 2021, il sera un des plus modernes de la région. À la demande du conseil de quartier de Ker-Uhel, et avec la validation des jeunes, la mairie a dégagé un plan d’investissement de 160 000 euros. «&nbsp;Nous l’avons placé en face du collège, pour créer un lieu de rencontre pour les jeunes de Ker-Uhel&nbsp;», explique Michel Lanchec.
+</p>
+	<div class="img-text">
+		<img src="../img/quartierspopulaires3.jpg" alt="Le chantier du parkour">
+		<div id="legende">
+			Le chantier du parkour de Ker-Uhel a débuté le 26 novembre.
+		</div>
+	</div>
+	
+<p>
+	Les premières pierres ont été posées fin novembre. Elles laissent de marbre Jessy et ses quatre amis. 
+</p>
+	
+<p>	
+<blockquote cite="http://www.worldwildlife.org/who/index.html">
+«&nbsp;<em>Le parkour ? On n’ira pas. Ce qu’on veut, c’est un nouveau city-stade. Et on leur a dit !</em>&nbsp;». 
+</blockquote>
+</p>
+<p>
+Le précédent, démoli en 2019, a laissé la place au nouveau collège Le-Goffic. Il n’y a pas eu de reconstruction depuis. L’installation d’un parkour a été privilégiée. Pour Françoise Squeren, ce parkour va servir de vitrine à la municipalité. <em>« J’ai l’impression que ça fait plus plaisir à la mairie qu’aux jeunes. Ils nous ont tous dit qu’ils préféraient un city-stade »</em>. Le responsable des sports, lui, défend fermement la position de la mairie. <em>« Le conseil de quartier ne voulait pas remettre un city-stade par rapport aux nuisances éventuelles. Le bruit, les ballons qui s’envolent… ».</em>
+</p>
+<p>
+Depuis la disparition de ce terrain multisports, les membres du conseil citoyen ont remarqué de nombreuses dégradations du mobilier urbain. Bien plus qu’avant. <em>« Des jeunes m’ont dit qu’ils avaient fait ça pour que la mairie les entende »</em>, explique Michaël Larifla, animateur quartier. En vain, rien n’étant arrivé aux oreilles de Michel Lanchec. Il a néanmoins conscience du problème. <em>« On sait que c’est un manque sur Ker-Uhel. On projette d’en réinstaller un mais la mairie n’a pas les fonds actuellement. Ce ne sera pas dans les deux ans à venir. Plutôt dans 5 à 8 ans. »</em> D’ici là, Jessy aura fêté sa majorité.
+</p>
