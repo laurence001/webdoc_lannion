7ac442a8ab3a2e3f31176a2450c8c0f1b54d0612
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-27 23:50:38 +0100

    Add files via upload

---
+++ corrections2501/content_violencessexuelles.php
@@ -0,0 +1,70 @@
+<p> 
+	Il est 18 heures lorsque Héloïse, 17 ans, arrive devant son club sportif à Lannion, le 13 septembre 2017. Caché derrière des haies, le gymnase accueille tous les jours des jeunes passionné.e.s de volleyball. La jeune fille en fait partie depuis maintenant trois ans. Sac à la main, genouillères enfilées, l’adolescente est pourtant réticente à l’idée de pousser la porte d’entrée de son club. Depuis plusieurs mois déjà, son entraîneur entretient des comportements déplacés envers elle et d’autres de ses coéquipières.  Une situation qui ne date pas d’hier.  L’homme exerçait aussi comme professeur de sport dans son collège, où elle avait déjà remarqué sa proximité avec certaines de ses élèves. Mais à l’époque, cela ne l’interrogeait pas plus que ça. 
+</p> 
+
+<p> 
+	Dans son cas, cela commence par des bracelets, offerts pendant les entraînements, et l’usage de surnoms comme <em>«&nbsp;ma chouchoute&nbsp;»</em>. L'entraîneur, qui possède aussi le numéro de téléphone des licencié.e.s, en profite ensuite pour lui envoyer des textos journaliers (<em>«&nbsp;Bonne journée&nbsp;», «&nbsp;ça va bébé?&nbsp;»...</em>). Puis il devient tactile, lui caresse la jambe lors des séances de musculations aux entraînements, lui fait des câlins par surprise, lui dit «&nbsp;<em> [qu'elle] a la peau douce </em>&nbsp;». </p>
+
+<p> 
+	«&nbsp;<em>Il faisait ça en rigolant mais j’en étais très mal à l’aise. Son attitude était malsaine  </em>&nbsp;», explique Héloïse. Très vite, la jeune fille se rend compte qu’elle n’est pas la seule dans cette situation. D’autres membres de son équipe s’avèrent être également victimes de ce comportement. </p>
+
+<h3 class="inter"><em>«&nbsp;Une coéquipière se faisait ramener et plus le temps passait plus elle avait peur&nbsp;» </em></h3>
+				
+<p> 
+	En en parlant entre elles dans les vestiaires après les entraînements, les adolescentes en arrivent à une prise de conscience collective. Certaines, comme Héloïse, arrêtent de lui parler en dehors des entraînements ; une décision compliquée étant donné la position de leur agresseur au sein du club. D’autres, décident de se taire, par peur de représailles. 
+</p> 
+
+<p>
+	Suite à ces comportements inacceptables, Héloïse et quelques-unes de ses amies quittent définitivement le club l’année suivante, en 2018. Un an plus tard, la parole se libère : l’entraîneur est remercié, et quitte l’association «&nbsp;<em> dans le silence et sans préjudices </em>&nbsp;» selon Paule, entraîneure au club. Dans son témoignage, Héloïse confie ne jamais avoir été informée de ce licenciement. </p>
+</p>
+
+<p> 
+	Hélas, elle et ses coéquipières ne sont pas les seules à avoir été victimes d’agressions ou de harcèlement sexuel au sein d’un club sportif à Lannion. 
+</p> 
+
+	<div class="genial">
+		<div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
+			<iframe frameborder="0" width="1200px" height="675px" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/5fff36e63127360d0e31ac90" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+		</div>
+	</div>
+<h3 class="inter"> <em>“Il s’est garé et m’a embrassé de force” </em></h3>   
+
+<p> 
+	En 2018, Valentine 3 débute sa première année d’études à Lannion. Déjà licenciée au club local de gymnastique rythmique, elle commence une nouvelle discipline : l'athlétisme. Comme sa résidence universitaire est loin du stade, son entraîneur propose de la ramener les soirs d'entraînements avec deux autres camarades. Au cours de l’année, ses deux coéquipier.e.s se blessent ; bientôt, elle se retrouve seule dans la voiture de son coach. L’homme adopte un comportement pervers envers la jeune femme de 20 ans, et la peur s’installe aussitôt. 
+	<blockquote>«&nbsp;<em>Le réflexe qu’il avait était de fermer à clé sa voiture. Donc j’étais seule, avec lui, voiture fermée. Tant qu’il n’ouvrait pas la voiture, je ne pouvais pas sortir. Un soir il m’a dit 'un jour je vais te faire un truc, et tu verras ce que ça va donner'.</em>&nbsp;» </blockquote>
+</p> 
+
+<p>
+	Peu de temps après, en 2019, la même situation se produit pour Lou, 17 ans,  licenciée  en athlétisme. Un soir, l'entraîneur propose de la ramener chez elle. Une fois arrivé.e.s, les portes de la voiture restent verrouillées. «&nbsp;<em> Il s’est garé et m’a embrassée de force </em>&nbsp;», se souvient-elle, choquée. Le soir-même, Lou appelle Agathe, une de ses amies proches, elle aussi licenciée au club, qui lui conseille d’en parler à ses parents. «&nbsp;<em> Je l’ai dit à mon père. Il était très énervé. Avec des amies, on s’était déjà plaintes de ces remarques déplacées, le club minimisait tout le temps&nbsp;»</em>, confie Lou. Le lendemain, la jeune fille se rend à l'entraînement, décidée à faire savoir ce qu’elle a subi la veille aux membres du club. «&nbsp;<em> On s’est réunis au stade pour, normalement, en parler entre nous </em>&nbsp;», explique-t-elle. «&nbsp;<em> Mon entraîneure a expliqué ce qu’il s’était passé, et a directement donné mon nom, sans mon accord. Puis au bout de 5 minutes, on a commencé un entraînement normal, comme si rien ne s’était passé [...] Je n’ai jamais eu de vraie discussion avec eux </em>&nbsp;», poursuit la jeune fille.
+</p>
+		
+<p> 
+	Lou décide alors de porter plainte, ce qui permet de libérer la parole de plusieurs autres victimes. «&nbsp;<em> On s’est toutes rendues compte qu’il y avait déjà énormément de plaintes contre lui, et que le club étouffait l'affaire. </em>&nbsp;» En fin d’année, certaines quittent le club, à contrecœur. Lou, elle, reste. «&nbsp;<em> Je suis inscrite mais pas vraiment pratiquante régulière </em>&nbsp;», explique-t-elle. La jeune fille confie cependant qu’aucune mesure n’a été mise en place suite aux agressions. 
+</p>
+<p>
+	La même année, l’agresseur est condamné à 18 mois de prison avec sursis pour des faits d’agressions sexuelles par le tribunal de Saint Brieuc. Contacté, le président du club lannionnais n’a pas souhaité s’exprimer sur l’affaire. 
+</p>
+<h3 class="inter"> Des failles dans la protection des adhérent.e.s </h3>
+<p> 
+	Licencié dans une association de football trégorroise, Vincent a lui aussi subi du harcèlement sexuel en 2017. «&nbsp;<em> Un entraîneur du club [...] m’a ajouté sur les réseaux sociaux puis m’a envoyé des messages très bizarres, du style : “est-ce que tu te branles?” , “est-ce que tu as déjà baisé ?”… </em>&nbsp;», explique-t-il.  Une attitude aussi inacceptable qu’incompréhensible. «&nbsp;<em> Je devais venir au club, mais il ne me connaissait pas, je ne sais pas comment il a eu mon Snapchat </em>&nbsp;». 
+</p>
+<p>
+	Au-delà des préjudices subis par Vincent, cette affaire met en lumière de nombreuses failles dans l’appréhension des violences et du harcèlement sexuel.le.s dans les clubs de sport. «&nbsp;<em> Nous n’avons pas communiqué l’affaire aux familles. C’est l’erreur qu’on a faite. C’est un non-dit alors que tout le monde est au courant </em>&nbsp;», témoigne Thierry, le président du club dans lequel joue Vincent. Depuis l’affaire de harcèlement sexuel de la part d’un de ses entraîneurs en 2017, les dirigeant.e.s se sont contenté.e.s de prévenir le club où l’agresseur s’est rendu après son licenciement. Et ce, parce que l’homme était déjà connu pour une affaire similaire dans un club de football de l’Argoat, en Bretagne. <em>" Avant d’arriver dans notre club, il y avait déjà eu des affaires comme ça dans un autre club, qui ne nous avait pas prévenu"</em>, explique Thierry. 
+</p>
+<p> 
+	La présidente de l’ancien club du harceleur reconnaît que des faits similaires s’étaient produits avec des adhérent.e.s âgé.e.s de 14 à 16 ans. «&nbsp;<em> Les jeunes nous ont prévenu et on l'a exclu du club </em>&nbsp;», explique-t-elle. Le harceleur décide ensuite d’intégrer un club en Bourgogne, mais rapidement, ses antécédents le rattrapent. «&nbsp;<em> On l’a dénoncé au club bourguignon </em> &nbsp;» , révèle la présidente du club de l’Argoat. «&nbsp;<em> Puis il est revenu dans le Trégor, dans un nouveau club. Là, on n’a pas fait suivre l’affaire car ce qu’il s’était passé dans notre association n’était pas avéré, ce n’était que des SMS “tendancieux”. </em>&nbsp;» 
+</p>
+
+<p> 
+	Dans les deux cas, aucun club n’avait vérifié les antécédents judiciaires du harceleur. «&nbsp;<em> En début de saison, on signe déjà une charte de "déclaration d’honorabilité” qui engage l’éducateur à témoigner du fait qu’il n’a pas de casier judiciaire et qu’il peut intervenir auprès des mineurs. </em>&nbsp;», révèle Thierry, le président du club trégorrois. «&nbsp;<em> On ne vérifie pas les casiers car on manque tellement de bénévoles, que dès qu’on a des volontaires on les accepte. </em>&nbsp;»
+</p>
+<h3 class="inter"> Les conséquences pour les clubs et les victimes  </h3>
+<p> 
+	«&nbsp;<em> Je m’effaçais dans l’équipe </em>&nbsp;» confie Héloïse, le cœur serré. Les violences subies il y a quatre ans sont encore gravées dans sa mémoire. Nombreux.se.s sont les licencié.e.s ayant cessé de pratiquer leur sport à cause d’un agresseur sexuel, sans forcément le dénoncer.  C’est le cas du club de volleyball lannionnais, où le nombre de licenciées a diminué après les faits de harcèlement sexuel. De 122 adhérentes inscrites sur la saison 2017-2018, les effectifs sont passés à 109 licenciées sur la saison 2019-2020.
+</p>
+<p> 
+	Le témoignage d’une entraîneure du club démontre que le comportement pervers de l’entraîneur pourrait bien être à l’origine de ces départs, même si les cas de violences et de harcèlements n’ont pas été mis en lumière. «&nbsp;<em> L’affaire, datant de 2017, est restée discrète au sein du club </em>&nbsp;», explique-t-elle. «&nbsp;<em> La baisse des licences est peut-être en lien avec [...] mais je ne peux pas l’affirmer. Évidemment, les filles ont parlé entre elles alors peut-être que certaines sont parties à cause de ça </em>&nbsp;». Le club de volley-ball n’est pas le seul à être concerné par cette diminution du nombre de licencié.e.s. Selon le témoignage d’un entraîneur du club de football trégorrois, «&nbsp;<em> quelques personnes [...] ont arrêté par rapport à cette histoire. </em>&nbsp;» 
+</p>
+<p> 
+	La majorité des victimes, ici, affirment n’en avoir parlé à aucun membre de leur famille, par peur de représailles. La boule au ventre, elles arrivent aujourd’hui avec difficulté à raconter ce qu’il leur est arrivé. Et pour cause, aucune aide n’est apportée pour permettre aux victimes de s’en sortir, dans les clubs sportifs locaux comme nationaux. Les agresseurs sexuels peuvent toujours entraîner, les dirigeant.e.s des clubs minimisent leur actes ou essaient d’oublier ce qu’il s’est passé en ne s’exprimant plus à ce sujet. Les clubs eux-mêmes ne cherchent pas à suivre les affaires passées, ne sachant même pas si les agresseurs exercent toujours dans ce milieu, avec des enfants et des adolescent.e.s. Selon nos informations, l’ancien entraîneur de volley-ball aurait par exemple entraîné pendant plusieurs mois dans un club breton, l’année suivant son renvoi, malgré les nombreuses accusations qui pesaient contre lui.  
+</p>
