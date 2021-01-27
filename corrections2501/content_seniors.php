6f6d5a7eac5ae4178ab28313e835929655502ace
ccrochu <71820023+ccrochu@users.noreply.github.com>
2021-01-27 14:07:17 +0100

    Add files via upload

---
+++ corrections2501/content_seniors.php
@@ -0,0 +1,67 @@
+<p>
+Par une fraîche matinée de novembre, la plage de Trestrignel devient un point de  rencontre des seniors. Iels ne sont pas adeptes des pratiques intenses, mais nombre d’entre elleux se baignent régulièrement en eau froide.
+</p>
+
+<p> <em>« Je vais dans l’eau toute l’année. À cette époque, elle est encore assez chaude pour  rester dix minutes. Dans un mois ce ne sera plus que cinq »</em>, explique Jean, habitant de Perros-Guirrec, à la carrure solide et à la peau fanée par l’âge. Avec sa compagne, Françoise, iels se  baignent deux fois par semaine, peu importe la température. Le couple, qui habite à  moins d’un kilomètre de la mer, a gardé ses habitudes matinales pendant le deuxième  confinement. <em>« En mars, les plages étaient interdites, c’était énervant, on se sentait  moins bien. C’est mauvais pour la circulation sanguine de ne pas bouger »</em>, déplore le vieil homme encore fringuant. <em>«  D’ailleurs notre médecin nous dit de continuer »</em>.
+</p> 
+				
+<h3 class="inter">10% des licencié·e·s ont plus de 60 ans  </h3>
+
+<p>En 2019, les fédérations françaises comptaient près de 2 millions de licenciée·s de plus de 60  ans. Rapporté au total des licencié·e·s français·e·s cette année-là, les seniors représentaient plus de 10% de l’effectif des associations et clubs sportif·ve·s français·e·s.  
+</p>
+
+<p>Alain Calvez fait partie de ces seniors actif·ve·s. Rien n’arrête ce féru de vélo. Pendant le  confinement, tous les deux jours, le sexagénaire parcourait 27 kilomètres, tout en restant dans le  périmètre d’un kilomètre autour de son domicile. <em>« C’est difficile d’avoir la volonté de faire ça tout seul, le groupe permet de sortir de son lit [NDLR, se motiver] »</em>. Parmi la quarantaine de licencié·e·s, <em>« la  majorité des membres du club a complètement arrêté »</em>, note le retraité.
+</p>
+
+<h3 class="inter">Des pratiques spécifiques et «&nbsp;bénéfiques&nbsp;» </h3>
+
+<p>Le nombre de licencié·e·s seniors invisibilise une partie des sportif·ve·s âgé·e·s. En effet, la majorité de  l’activité physique des personnes âgées ne se fait ni en club, ni dans une association. La marche, la course à pied ou la natation sont des pratiques privilégiées par les seniors, sans grande répercussion dans les chiffres des fédérations. La Fédération française de natation compte pas moins de 512 015 licencié·e·s. Et pour cause, l’accès aux infrastructures n’est pas réservé aux clubs. 
+</p>
+
+<p> En 2009, 16% des Français·e·s entre 70 et 75 ans déclaraient faire partie d’un club ou une  association sportive. Proportion assez faible quand on sait que 34% d’entre elleux faisaient  de l’activité sportive de manière spontanée, pas à la portée de tou·te·s. 
+</p>
+
+<p>Karl Chaory, docteur en médecine de réadaptation à Rennes, vante les bénéfices de l’activité physique en groupe chez les publics fragiles.
+</p>
+
+<blockquote cite="http://www.worldwildlife.org/who/index.html">
+« Les bienfaits du sport touchent la  partie cardio-respiratoire. La pratique sportive apportent aussi des avantages psychologiques et psychosociaux .»
+</blockquote>
+
+<p>Le sport permet également de développer une plus grande résistance contre la douleur. Le spécialiste poursuit.Selon lui, c'est aussi une question d’ <em>« amélioration du sommeil et de l’hygiène de vie, parce qu’à partir du moment ou l’on fait du sport, on fait attention à ce que l’on va manger »</em>. Le médecin ne recommande pas de pratique intense, mais de la marche, du vélo ou du renforcement musculaire. Des exercices que l’on peut faire sans club de sport, sans infrastructure spécifique. 
+</p>
+<div id="demo" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+			  <div class="carousel-item active">
+				<img src="../img/caroussel_seniors1.jpg" alt="Bretagne" class="d-block w-100">
+			  </div>
+			  
+			  <div class="carousel-item">
+				<img src="../img/caroussel_seniors2.jpg" alt="Bretagne" class="d-block w-100">
+			  </div>
+			  
+			  <div class="carousel-item">
+				<img src="../img/caroussel_seniors3.jpg" alt="Bretagne" class="d-block w-100">
+			  </div>
+			  
+			  <div class="carousel-item">
+				<img src="../img/caroussel_seniors4.jpg" alt="Bretagne" class="d-block w-100">
+			  </div>
+			  
+			  <div class="carousel-item">
+				<img src="../img/caroussel_seniors5.jpg" alt="Bretagne" class="d-block w-100">
+			  </div>
+		</div>
+	</div>
+		
+	<div id="legende">
+		Marche, surf, longe-côte, nage, course à pied… Les personnes âgées profitent du dimanche matin pour se dégourdir les jambes à Perros-Guirec.
+	</div>
+<h3 class="inter">Oublié·e·s parmi les oublié·e·s </h3>
+<p> D'autres seniors manquent cruellement d'exercice. Présenté·e·s comme les plus fragiles, les résident·e·s d'Établissement d'Hébergement pour Personnes  gées Dépendantes (EHPAD) ont été privé·e·s de mouvement. Confiné·e·s pendant plusieurs mois dans des chambres d’une dizaine de mètres carrés, les seniors ne pouvaient pas se rendre dans les  espaces collectifs. Le service à la chambre a été imposé à des milliers de pensionnaires privé·e·s d’activités de groupe. À la fin du confinement, <em>« certain·e·s ne savent plus marcher, on voit bien qu’on aura besoin de plus de prise en charge par des kinésithérapeutes »</em>, se désole Florence Braud, aide-soignante dans un établissement morbihannais. C’est l’un des symptômes d’une  désadaptation psycho-motrice, un syndrome dû au manque d’activité. <em>« Quand nos  résident·e·s ne pratiquent plus certains gestes pendant un moment, iels en perdent l’habitude »</em>, explique Jacky Fichaux. 
+</p>
+<blockquote cite="http://www.worldwildlife.org/who/index.html">
+« Comme les personnes sont isolées dans leurs chambres, il faut plus de mains  pour les repas, il faut plus de mains pour stimuler les résidents », poursuit l’infirmier cadre dans un EHPAD de Lille.</blockquote>
+<p> Si le Nordiste  affirme avoir eu une rallonge de l’Agence Régionale de Santé pour du personnel en  renfort, sa consoeur y voit un problème. <em>« Dans certains EHPAD il y a eu des renforts,  d’autres non, ça dépendait des régions »</em>, observe Florence Braud, avant de raccrocher pour se rendre  au chevet d’un pensionnaire tombé. La majorité des établissements contactés avaient mis leurs animations sportives et culturelles en pause. Les associations et intervenants externes, habituellement chargé·e·s de s’occuper des résident·e·s, trouvent aujourd’hui encore « porte  close ». Et pourtant,  les pensionnaires sont en demande. Marie-Louise Laurent réside à l’EHPAD de Neussargues-en-Pinatelle, dans le Cantal. <em>« On est plus agiles grâce à la  gym, on se sent mieux »</em>, résume la pensionnaire de quatre-vingt-six ans <em>« et demi »</em>, comme  elle tient à le préciser. 
+</p>
+<p> Les mesures de confinement décrétées par le gouvernement depuis mars avaient pour but de limiter les risques de contamination au coronavirus. Officiellement, les personnes âgées sont une priorité. Pourtant, les conséquences de l’absence de pratique sportive chez les seniors, à la fois sur leur santé mentale et  physique, pourraient avoir été sous-estimée. En voulant protéger la qualité de vie de nos aîné·e·s, c’est peut-être l’inverse qui s’est produit. </p>
